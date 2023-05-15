52905f20560e83d7b3be955c1b9d3b2cbc8c751d
vikash05/07/1996 <vikashrajpoot2995@gmail.com>
2023-05-15 16:15:14 +0530

    serializers

--- api1/views.py
+++ api1/views.py
@@ -9,18 +9,12 @@ from django.http import HttpResponse
 # Create your views here.
 def Student_detail(request,v):
     stu = Student.objects.get(id=v)
-    print(stu,"reading stu")
     serializer =StudentSerializers(stu)
-    print(serializer)
     json_data = JSONRenderer().render(serializer.data)
-    print(json_data)
     return HttpResponse(json_data)
 def Student_list(request):
     stu = Student.objects.all()
-    print(stu,"reading stu")
     serializer =StudentSerializers(stu,many=True)
-    print(serializer)
     json_data = JSONRenderer().render(serializer.data)
-    print(json_data)
     return HttpResponse(json_data)
 
